1c4eaf713f23341d821443ae2950c6660c9b9920
Reshait <reshait@outlook.com>
2016-03-18 14:21:39 +0100

    Falla comparación de donantes al introducir un valor no válido en grupo y factor

--- practica2/donante.hpp
+++ practica2/donante.hpp
@@ -9,6 +9,8 @@ using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
+using std::istream;
+using std::ostream;
 
 class Donante: public ed::DonanteInterfaz {
     private:
@@ -25,33 +27,55 @@ class Donante: public ed::DonanteInterfaz {
         
         void setNombre(string nombre){ nombre_ = nombre; }
         void setApellidos(string apellidos){ apellidos_ = apellidos; }
-        void setGrupo(string grupo){ grupo_ = grupo; }        
-        void setFactor(string factor){ factor_ = factor; }    
+        void setGrupo(string grupo){
+        	while(grupo != "0" && grupo != "A" && grupo != "B" && grupo != "AB" && grupo != "@"){
+        		cout << "El Grupo sanguíneo no puede ser distinto de 0|A|B|AB." << endl;
+        		cout << "Por favor Introduzca de nuevo el Grupo sanguíneo ..: ";
+        		cin >> grupo;
+        	}
+        	grupo_ = grupo; 
+        }        
+        void setFactor(string factor){ 
+        	while(factor != "positivo" && factor != "Positivo" && factor != "POSITIVO" && factor != "negativo" && factor != "Negativo" && factor != "NEGATIVO" && factor != "@@@@@@@@"){
+        		cout << "El Factor RH no puede ser distinto de 'Positivo' o 'Negativo'." << endl;
+        		cout << "Por favor Introduzca de nuevo el Factor RH ..: ";
+        		cin >> factor;
+        	}
+        	factor_ = factor; 
+        }    
         
-        Donante(string nombre = "XXXXX", string apellidos = "XXXXX XXXXX", string grupo = "X", string factor = "XXXXXXXX"){
+        Donante(string nombre = "XXXXX", string apellidos = "XXXXX XXXXX", string grupo = "@", string factor = "@@@@@@@@"){
         	setNombre(nombre);
         	setApellidos(apellidos);
         	setGrupo(grupo);
         	setFactor(factor);  
         }   
         
-        Donante(const Donante &D);
+        Donante(const Donante &D){
+        	setNombre(D.getNombre());
+        	setApellidos(D.getApellidos());
+        	setGrupo(D.getGrupo());
+        	setFactor(D.getFactor());
+        }
+
         //¿Acceso y modificación?
+       	//Si hay problemas usar getchar();       	
         void leeDonante(){
         	string nombre, apellidos, grupo, factor;
         	cout << "Introduzca el nombre del donante\t..: ";
+        	//getchar();
         	getline(cin, nombre);
         	cout << "Introduzca los apellidos del donante\t..: ";
         	getline(cin, apellidos);
-        	cout << "Introduzca el grupo sanguíneo\t..: ";
+        	cout << "Introduzca grupo sanguíneo (0|A|B|AB)\t..: ";
         	getline(cin, grupo);
-        	cout << "Introduzca el factor RH\t\t..: ";
+        	cout << "Introduzca factor RH (positivo|negativo)..: ";
         	getline(cin, factor);
-        	cout << "==========================================" << endl;
+
         	setNombre(nombre);
         	setApellidos(apellidos);
         	setGrupo(grupo);
-        	setFactor(factor);        	
+        	setFactor(factor); 
         }
         
         void escribeDonante(){
@@ -61,7 +85,57 @@ class Donante: public ed::DonanteInterfaz {
         	cout << "\tGrupo Sanguíneo\t..: " << getGrupo() << endl;
         	cout << "\tFactor RH\t..: " << getFactor() << endl;
         }
-        
+
+		void modificaDatosDonante(){
+			int opcion;
+			string cadena;
+
+			do{
+				system("clear");
+
+				cout << "1.- Para modificar el Nombre." << endl;
+				cout << "2.- Para modificar los Apellidos." << endl;
+				cout << "3.- Para modificar el Grupo sanguíneo." << endl;
+				cout << "4.- Para modificar el Factor RH." << endl;
+				cout << "0.- Para Salir." << endl;
+				cout << "\tIntroduzca una opción >> ";
+				cin >> opcion;
+				getchar();
+
+				switch(opcion){
+					case 1: 
+						cout << "El nombre actual del donante es\t ..: " << getNombre() << endl;
+						cout << "Introduzca el nuevo nombre\t ..: ";
+						getline(cin, cadena);
+						setNombre(cadena);
+						break;
+					case 2:
+						cout << "Los apellidos actuales son\t ..: " << getApellidos() << endl;
+						cout << "Introduzca los nuevos apellidos\t ..: ";
+						getline(cin, cadena);
+						setApellidos(cadena);
+						break;
+					case 3:
+						cout << "El Grupo sanguíneo actual es (0|A|B|AB)\t ..: " << getGrupo() << endl;
+						cout << "Introduzca el nuevo grupo sanguíneo\t ..: ";
+						getline(cin, cadena);
+						setGrupo(cadena);
+						break;
+					case 4:
+						cout << "Factor RH actual (positivo|negativo)\t ..: " << getFactor() << endl;
+						cout << "Introduzca el nuevo factor RH\t\t ..: ";
+						getline(cin, cadena);
+						setFactor(cadena);
+						break;
+					case 0:
+						cout << "Saliendo..." << endl;
+						break;	
+					default:
+						cout << "Opción incorrecta." << endl;		
+				}
+			}while(opcion != 0);
+		}
+
         Donante &operator=(const Donante &D){
         	this->setNombre(D.getNombre());
         	this->setApellidos(D.getApellidos());
@@ -85,6 +159,35 @@ class Donante: public ed::DonanteInterfaz {
         	}
 
         	return devuelve;
+        }
+
+        friend istream &operator>>(istream &salida, Donante &D){
+        	string nombre, apellidos, grupo, factor;
+        	cout << "Introduzca el nombre del donante\t..: ";
+        	getline(salida, nombre);
+        	cout << "Introduzca los apellidos del donante\t..: ";
+        	getline(salida, apellidos);
+        	cout << "Introduzca el grupo sanguíneo\t..: ";
+        	getline(salida, grupo);
+        	cout << "Introduzca el factor RH\t\t..: ";
+        	getline(salida, factor);
+        	cout << "==========================================" << endl;
+        	D.setNombre(nombre);
+        	D.setApellidos(apellidos);
+        	D.setGrupo(grupo);
+        	D.setFactor(factor); 
+
+        	return salida;
+        }
+
+        friend ostream &operator<<(ostream &entrada, Donante &D){
+       		entrada << "==========================================" << endl;
+        	entrada << "\tNombre\t\t..: " << D.getNombre() << endl;
+        	entrada << "\tApellidos\t..: " << D.getApellidos() << endl;
+        	entrada << "\tGrupo Sanguíneo\t..: " << D.getGrupo() << endl;
+        	entrada << "\tFactor RH\t..: " << D.getFactor() << endl;  	
+
+        	return entrada;
         }
 };
 
--- practica2/main.cpp
+++ practica2/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib> // Para los systems del menú en linux
 #include "donanteinterfaz.hpp"
 #include "donante.hpp"
+#include "menuDonante.hpp"
 
 using namespace std;
 
@@ -10,25 +11,13 @@ using namespace std;
 int main(){
 	system("clear");
 
-	Donante D1, D2;
-	D1.leeDonante();
-	D2.leeDonante();
-	D1.escribeDonante();
-	D2.escribeDonante();
+	Donante D1("Teófilo", "Rojas Mata", "A", "positivo"), D2(D1), D3;
 
-/*
-	string antonio = "antonio", pedro = "pedro";
-	if( pedro >= antonio)
-		cout << "Están ordenados" << cout << endl;
-	else
-		cout << "No están ordenados" << cout << endl;
-*/
-	if(D1 <= D2){
-		cout << D1.getApellidos() << " " << D1.getNombre() << " está antes alfabéticamente que ";
-		cout << D2.getApellidos() << " " << D2.getNombre() << endl;
-	}
-	else
-		cout << "Mal ordenados" << endl;
+	menu(D1, D2, D3);
+
+
+	///////////////////////////////////////
+
 	return 0;
 }
 
